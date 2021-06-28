3d9526801b0d62530a8ecd9416915d9b0a95974e
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2021-06-27 23:30:51 -0300

    fix end-bar

--- index.js
+++ index.js
@@ -25,15 +25,3 @@ views = ["10K PAGEVIEWS", "50K PAGEVIEWS", "100K PAGEVIEWS", "500k PAGEVIEWS", "
 
 prices = [8, 12, 16, 24, 36];
 
-
-
-    /* value = [];/ $8 per month
- / $12 per month
- / $16 per month
-prices = ["8.00", "12.00", "16.00", "24.00", "36.00"];
- / $24 per month
- / $36 per month
-if (antvalue != range.value) {
-    cantviews.innerHTML=views[range.value];
-    antvalue = range.value;
-}*/
